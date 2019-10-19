ae3d71dac0155d95da0daed59528c37888b384e7
nnamm <t.nnamm@gmail.com>
2019-10-19 23:38:12 +0900

    Add画面、Firestoreへのデータ追加機能の実装完了

--- day1/src/views/Add.vue
+++ day1/src/views/Add.vue
@@ -37,28 +37,28 @@
           label-cols-md="3"
           label="ID">
           <b-form-input
-            v-model="id"
+            v-model="innerId"
             type="number" />
         </b-form-group>
         <b-form-group
           label-cols-md="3"
           label="Title">
           <b-form-input
-            v-model="title"
+            v-model="innerTitle"
             type="text" />
         </b-form-group>
         <b-form-group
           label-cols-md="3"
           label="Tags">
           <b-form-input
-            v-model="tags"
+            v-model="innerTags"
             type="text" />
         </b-form-group>
         <b-form-group
           label-cols-md="3"
           label="url">
           <b-form-input
-            v-model="url"
+            v-model="innerUrl"
             type="text" />
         </b-form-group>
       </b-col>
@@ -77,55 +77,41 @@
   </b-container>
 </template>
 
-// TODO: daysにpushするロジックを作る
-// doc → id, title、tags, url, display=true
-// tagsは「,」区切りで追加する
-// それを分割してオブジェクトに追加するロジックを組む
-// ↓objはこんな感じ
-// {
-//   id: id,
-//   title: title,
-//   tags: [],
-//   url: url,
-//   display: true
-// }
-
 <script>
 import { auth } from '@/firebase.js'
 import { db } from '@/firebase.js'
 
 export default {
   name: 'Add',
-  data() {
-    return {
-      id: '',
-      title: '',
-      tags: '',
-      tagsArray: [],
-      url: '',
-    }
-  },
+  data: () => ({
+    innerId: '',
+    innerTitle: '',
+    innerTags: '',
+    innerUrl: '',
+  }),
   created: function() {
     this.user = auth.currentUser
   },
   methods: {
     addContent: function(){
-      const me = this
-      const doc = {
-        id: Number(this.id),
-        title: this.title,
-        tags: this.tagsArray,
-        url: this.url,
-        display: true
+      const docName = 'day' + this.innerId
+      const result = confirm('Add or Update a "' + docName + '" realy OK?')
+      if (result) {
+        const docContent = {
+          id: Number(this.innerId),
+          title: this.innerTitle,
+          tags: this.innerTags.split(','),
+          url: this.innerUrl,
+          display: true
+        }
+        db.collection('days').doc(docName).set(docContent, {merge: true})
+        .then(() => {
+          alert('Addition complete')
+        })
+        .catch((err) => {
+          console.error('Error writing document: ', err)
+        })
       }
-      // TODO: ドキュメント名の指定方法は？
-      // TODO: meはundefinedになる
-      db.collection('days').add(doc)
-      .then((docref) => {
-        me.days.push(doc)
-        alert('Addition complete')
-        console.log(docref)
-      })
     },
     logOut: function(){
       auth.signOut().then(() => {
